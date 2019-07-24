f52bef11f12dbafcc5ce432f363b862490acb186
veerasarma <veerasarma@osiztechnologies.com>
2019-07-24 20:01:33 +0530

    initial

---
+++ truffle-config.js
@@ -0,0 +1,106 @@
+require('dotenv').config();
+require('babel-register');
+require('babel-polyfill');
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const providerWithMnemonic = (mnemonic, rpcEndpoint) => () =>
+  new HDWalletProvider(mnemonic, rpcEndpoint);
+
+const infuraProvider = network => providerWithMnemonic(
+  process.env.MNEMONIC || '',
+  `https://${network}.infura.io/v3/${process.env.INFURA_API_KEY}`
+);
+
+const ropstenProvider = process.env.SOLIDITY_COVERAGE
+  ? undefined
+  : infuraProvider('ropsten');
+
+/*module.exports = {
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 7545,
+      network_id: '*', // eslint-disable-line camelcase
+      gasPrice: 0x01,
+    },
+    ropsten: {
+      provider: ropstenProvider,
+      network_id: 3, // eslint-disable-line camelcase
+      gasPrice: 5000000000,
+    },
+    coverage: {
+      host: 'localhost',
+      network_id: '*', // eslint-disable-line camelcase
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
+    },
+    ganache: {
+      host: 'localhost',
+      port: 7545,
+      network_id: '*', // eslint-disable-line camelcase
+    },
+    dotEnvNetwork: {
+      provider: providerWithMnemonic(
+        process.env.MNEMONIC,
+        process.env.RPC_ENDPOINT
+      ),
+      network_id: parseInt(process.env.NETWORK_ID) || '*', // eslint-disable-line camelcase
+    },
+  },
+  compilers: {
+    solc: {
+      version: '0.5.10',
+      settings: {
+        optimizer: {
+          enabled: true, // Default: false
+          runs: 0, // Default: 200
+        },
+      },
+    },
+  },
+};*/
+
+ const mnemonic = "embark useful neither demise myth lecture damp mouse bless lava convince double";
+
+
+module.exports = {
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*' // Match any network id
+    },
+    // testnets
+    // properties
+    // network_id: identifier for network based on ethereum blockchain. Find out more at https://github.com/ethereumbook/ethereumbook/issues/110
+    // gas: gas limit
+    // gasPrice: gas price in gwei
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/56b5bbcb78c849da844733c14746e16e"),
+      network_id: 3
+      // gas: 500000,
+      // gasPrice: 10000000000
+    },
+    kovan: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 42,
+      gas: 3000000,
+      gasPrice: 10000000000
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider("embark useful neither demise myth lecture damp mouse bless lava convince double", "https://rinkeby.infura.io/v3/56b5bbcb78c849da844733c14746e16e"),
+      network_id: 4,
+      gas: 3000000,
+      gasPrice: 10000000000
+    },
+    // main ethereum network(mainnet)
+    main: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 1,
+      gas: 3000000,
+      gasPrice: 10000000000
+    }
+  }
+};
